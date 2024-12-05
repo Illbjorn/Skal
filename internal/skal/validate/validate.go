1c08b4e95995a08c819a19ffa7227c4073cb2bb8
Illbjorn <am@hades.so>
2024-12-05 04:57:34 -0500

    Linter Issues

--- internal/skal/validate/validate.go
+++ internal/skal/validate/validate.go
@@ -132,14 +132,20 @@ func validateCall(call *typeset.Call) {
 			"fn", call.Ref(),
 		)
 		verr(err, call.Token())
+		return
+	}
+
+	if kt.object == nil {
+		err := fstr.Pairs(
+			"Failed to located called fn: {fn}.",
+			"fn", call.Ref(),
+		)
+
+		verr(err, call.Token())
+		return
 	}
 
 	fn := kt.object.(*typeset.Fn)
-
-	// Confirm the number of args match.
-	if len(call.Args) != len(fn.Args) {
-
-	}
 
 	// Validate arg types match.
 	for _, callArg := range call.Args {
